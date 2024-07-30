47697bbcb74249d6d39b4cdd52347a7be49e1be2
gaurishmalhotra <gaurishmalhotra2005@gmail.com>
2024-07-30 23:44:47 +0530

    Add files via upload

---
+++ hello2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    int d;
+    cout<<"Enter the value of a and b: "<<endl;
+    cin>>a;
+    cin>>b;
+    d = a + b;
+    cout<<"The Sum of a and b is: "<<d<<endl;
+   //cout<<"First Program!"<<endl;
+   //cout<<"Hello World"<<endl;
+}
