b9c21599da88defa7281ab8dbbfefaae45d4f40c
gaurishmalhotra <gaurishmalhotra2005@gmail.com>
2024-07-30 23:54:16 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    int d;
+    cout<<"Enter the value of a and b: "<<endl;
+    cin>>a;
+    cin>>b;
+    d = a + b;
+    cout<<"The Sum of a and b is: "<<d<<endl;
+}
